17cfd68e1d80e0f2aca3f84f1d7af2c0d9ecedc3
Kelvin <machakakelvin903@gmail.com>
2024-11-29 22:24:38 +0200

    Update allowedOrigins.js with new URL

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,7 @@
-// Define an array of allowed origins (domains) that are permitted to access server resources
+
 const allowedOrigins = [
-        
-    'https://www.technotes.onrender.com',  
-         
+    'https://technotes-urh5.onrender.com',  
 ];
 
-// Export the array so it can be used in the CORS configuration
+
 module.exports = allowedOrigins;
